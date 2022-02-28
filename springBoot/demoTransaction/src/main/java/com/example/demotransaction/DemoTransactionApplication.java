51e1de2beff98f2290de7740182b365bb388a7f2
Hai TRAN <tranvanhoanghai2510@gmail.com>
2022-02-28 23:03:46 +0700

    source demo transaction

---
+++ springBoot/demoTransaction/src/main/java/com/example/demotransaction/DemoTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTransactionApplication.class, args);
+    }
+
+}
---
+++ springBoot/demoTransaction/src/test/java/com/example/demotransaction/DemoTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
